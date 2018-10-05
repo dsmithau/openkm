fdc1940d609e2323d2c482e1cc7759b564e0babf
dsmithau <dsmith.au@gmail.com>
2018-10-05 11:20:57 +0800

    Final update for remote database connectivity

---
+++ create_container.sh
@@ -0,0 +1,1 @@
+docker run --env-file env.list --privileged=true -d --name=openkm $1
--- start.sh
+++ start.sh
@@ -4,10 +4,10 @@
 service nfs-common start
 service rpcbind start
 
-sed -i 's/MYSQL_SERVER_HOST/'"${MYSQL_SERVER_HOST}"'/g' /home/openkm/tomcat-8.5.24/conf/server.xml && \
+sed -i 's/MYSQL_SERVER_HOST/'"${MYSQL_SERVER_HOST}"'/g' /home/openkm/tomcat-8.5.24/conf/server.xml 
 sed -i 's/MYSQL_SERVER_PORT/'"${MYSQL_SERVER_PORT}"'/g' /home/openkm/tomcat-8.5.24/conf/server.xml
+sed -i 's/MYSQL_SERVER_PASSWORD/'"${MYSQL_SERVER_PASSWORD}"'/g' /home/openkm/tomcat-8.5.24/conf/server.xml 
 sed -i 's/MAIL_HOST/'"${MAIL_HOST}"'/g' /home/openkm/tomcat-8.5.24/conf/server.xml
-
 
 
 mount -t nfs ${NFS_SERVER_SERVER}:${NFS_SERVER_EXPORT_PATH} ${LOCAL_NFS_MOUNT_POINT}
